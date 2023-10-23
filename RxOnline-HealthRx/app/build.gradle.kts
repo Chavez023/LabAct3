459e4a7677e5a57c9ca8b2ff8819f04a15cc408c
Chavez <nimbus.arc@outlook.com>
2023-10-23 18:41:30 +0800

    Removed from the app directory

--- RxOnline-HealthRx/app/build.gradle.kts
+++ RxOnline-HealthRx/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.healthrx"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.healthrx"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
