a713883a24cbef91c31148128fe8feaee79d5597
Chavez <nimbus.arc@outlook.com>
2023-11-10 20:34:59 +0800

    Fixed Errors

--- RxOnline-HealthRx/app/src/main/java/com/example/infirmary/SignIn.kt
+++ RxOnline-HealthRx/app/src/main/java/com/example/infirmary/SignIn.kt
@@ -1,11 +1,10 @@
 package com.example.infirmary
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.infirmary.databinding.ActivitySignInBinding
-import com.example.infirmary.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SignIn : AppCompatActivity() {
